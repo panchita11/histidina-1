2f60ed536a3d1cfec5b2f62c29c1499670f4e2c6
panchita11 <teran110805@gmail.com>
2024-11-20 15:15:06 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,5 @@ opcion = st.sidebar.selectbox(
 # Contenido dinámico basado en la selección
 if opcion == "Información básica":
     st.header("Información básica sobre la Histidina")
-    st.write("""
-    La histidina es un aminoácido esencial que desempeña un papel crucial en 
-    varias funciones biológicas. Su fórmula química es **C₆H₉N₃O₂** y es conocida 
-    por ser precursora de la histamina.
-    """)
-    st.wride("""En la naturaleza encontramos el enantiómero levorrotarorio de la histidina, es decir, la forma L-histidina.
-
-Tanto de origen animal como vegetal, por lo que su carencia es una condición extraña y normalmente asociada a alteraciones genéticas o malnutrición severa.""")
-
-    st.image("https://arribasalud.com/wp-content/uploads/2018/02/histidina-665x285.jpg")
+    st.write("""La histidina (His, H) es un aminoácido que se utiliza para la síntesis de proteínas. Es una molécula hidrofílica, por lo que generalmente está orientada hacia la parte externa de las estructuras proteicas cuando estas se encuentran en un medio líquido.""")
+ 
